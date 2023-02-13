319b9d9af465dcb2785ef708228a99bdbc634cc2
Eva Capecci <ecapecci@segurosrivadavia.com>
2023-02-13 14:22:50 -0300

    Fix producerProperties

--- src/main/java/com/mcapecci/cursokafkaspring/configuration/KafkaConfiguration.java
+++ src/main/java/com/mcapecci/cursokafkaspring/configuration/KafkaConfiguration.java
@@ -6,6 +6,7 @@ import java.util.Map;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
+import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
@@ -30,8 +31,8 @@ public class KafkaConfiguration {
 	public Map<String, Object> producerProperties() {
 		Map<String, Object> props = new HashMap<>();
 		props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
-		props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringDeserializer.class);
-		props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+		props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+		props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
 		return props;
 
 	}
