dff0812b0a2cc3fb6d74fc3ca8fe17504727875b
Eva Capecci <ecapecci@segurosrivadavia.com>
2023-02-13 14:12:35 -0300

    Se agrega Producer

--- src/main/java/com/mcapecci/cursokafkaspring/CursoKafkaSpringApplication.java
+++ src/main/java/com/mcapecci/cursokafkaspring/CursoKafkaSpringApplication.java
@@ -2,21 +2,32 @@ package com.mcapecci.cursokafkaspring;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.core.KafkaTemplate;
 
 @SpringBootApplication
-public class CursoKafkaSpringApplication {
+public class CursoKafkaSpringApplication implements CommandLineRunner {
 	private static final Logger log = LoggerFactory.getLogger(CursoKafkaSpringApplication.class);
-	
+
+	@Autowired
+	private KafkaTemplate<String, String> kafkaTemplate;
+
 	public static void main(String[] args) {
 		SpringApplication.run(CursoKafkaSpringApplication.class, args);
 	}
-	
+
 	@KafkaListener(topics = "mcapecci-topic", groupId = "mcapecci-group")
 	public void listen(String message) {
-	    log.info("Message Received: " + message);
+		log.info("Message Received: " + message);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		kafkaTemplate.send("mcapecci-topic", "sample message");
 	}
 
 }
--- src/main/java/com/mcapecci/cursokafkaspring/configuration/KafkaConfiguration.java
+++ src/main/java/com/mcapecci/cursokafkaspring/configuration/KafkaConfiguration.java
@@ -4,12 +4,15 @@ import java.util.HashMap;
 import java.util.Map;
 
 import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
 
 @Configuration
 public class KafkaConfiguration {
@@ -24,20 +27,33 @@ public class KafkaConfiguration {
 
 	}
 
+	public Map<String, Object> producerProperties() {
+		Map<String, Object> props = new HashMap<>();
+		props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+		props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+		props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+		return props;
+
+	}
+
+	@Bean
+	public KafkaTemplate<String, String> kafkaTemplate() {
+		DefaultKafkaProducerFactory producerFactory = new DefaultKafkaProducerFactory<>(producerProperties());
+		KafkaTemplate<String, String> template = new KafkaTemplate<>(producerFactory);
+		return template;
+	}
+
 	@Bean
 	public ConsumerFactory<String, String> consumerFactory() {
 		return new DefaultKafkaConsumerFactory<>(consumerProperties());
 	}
-	
+
 	@Bean
-    public ConcurrentKafkaListenerContainerFactory<String, String> 
-     listenerContainerFactory() {
-   
-        ConcurrentKafkaListenerContainerFactory<String, String> listenerContainerFactory =
-          new ConcurrentKafkaListenerContainerFactory<>();
-        listenerContainerFactory.setConsumerFactory(consumerFactory());
-        return listenerContainerFactory;
-    }
-	
+	public ConcurrentKafkaListenerContainerFactory<String, String> listenerContainerFactory() {
+
+		ConcurrentKafkaListenerContainerFactory<String, String> listenerContainerFactory = new ConcurrentKafkaListenerContainerFactory<>();
+		listenerContainerFactory.setConsumerFactory(consumerFactory());
+		return listenerContainerFactory;
+	}
 
 }
